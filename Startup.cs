e840bbd9dd12303fe084f3fbce551c6a6afb25d7
Quynh Nguyen <mr.quynh@gmail.com>
2025-10-28 05:32:05 +0700

    (improv): logging paths

--- Startup.cs
+++ Startup.cs
@@ -69,7 +69,7 @@ namespace net.vieapps.Services.SRP
 			var loggerFactory = appBuilder.ApplicationServices.GetService<ILoggerFactory>();
 			var logPath = UtilityService.GetAppSetting("Path:Logs");
 			if (!string.IsNullOrWhiteSpace(logPath) && Directory.Exists(logPath))
-				loggerFactory.AddFile(logPath, $"{Global.ServiceName}.http.pid_{Environment.ProcessId}");
+				loggerFactory.AddFile(logPath, $"{Global.ServiceName}.http.{Environment.ProcessId}");
 			else
 				logPath = null;
 
